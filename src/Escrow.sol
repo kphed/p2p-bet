dfba3ef1f7329eaf98666d706c16a8bf3b7a16ad
kphed <kpcrypto69@gmail.com>
2023-03-17 16:55:49 -0400

    Define maximum escrow funding params

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -11,4 +11,10 @@ contract Escrow {
         ERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
     ERC20 public constant USDC =
         ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+
+    // 2 WBTC (ERC20 token has 8 decimals, so 2e8 is 2 WBTC)
+    uint256 public constant MAX_WBTC = 2e8;
+
+    // 2,000,000 USDC (ERC20 token has 6 decimals, so 2_000_000e6 is 2,000,000 USDC)
+    uint256 public constant MAX_USDC = 2_000_000e6;
 }
